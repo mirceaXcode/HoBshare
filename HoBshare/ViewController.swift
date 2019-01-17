3ed9374e78ec110c6aac879e84813b0156d08994
Mircea Popescu <mirceap@gmail.com>
2019-01-17 13:58:38 -0500

    Initial Commit

---
+++ HoBshare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HoBshare
+//
+//  Created by Mircea Popescu on 1/17/19.
+//  Copyright © 2019 Mircea Popescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
